f8e07bc2ed6de485e2c8d87cfa64031356524300
Vikram Kapoor <vikram@lacework.net>
2015-10-21 09:39:49 -0700

    Use memmove instead of memcpy

--- pcap.go
+++ pcap.go
@@ -12,7 +12,9 @@ package pcap
 
 void *__memcpy_glibc_2_2_5(void *, const void *, size_t);
 
-asm(".symver __memcpy_glibc_2_2_5, memcpy@GLIBC_2.2.5");
+// We use memmove rather than memcpy here since it is safer
+// for overlapping memory regions.
+asm(".symver __memcpy_glibc_2_2_5, memmove@GLIBC_2.2.5");
 void *__wrap_memcpy(void *dest, const void *src, size_t n)
 {
     return __memcpy_glibc_2_2_5(dest, src, n); 
